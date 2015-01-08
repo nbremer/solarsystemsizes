967df4de33afb12f4cf7ac9ec3c35772a3362fbd
nbremer <nadiehbremer@gmail.com>
2015-01-08 21:15:59 +0100

    First commit

---
+++ js/helperFunctions.js
@@ -0,0 +1,118 @@
+//////////////////////////////////////////////////////
+/////////////// Helper Functions /////////////////////
+//////////////////////////////////////////////////////
+window.onerror = function() {
+    location.reload();
+}
+     	   
+function tick(e) {
+  circle
+	  .each(cluster(10 * e.alpha * e.alpha))
+	  .each(collide(0.25))
+	  .attr("cx", function(d) { return (d.x); })
+	  .attr("cy", function(d) { return (d.y); });
+}//tick
+
+//Show the planet name on mouseover of planet/Sun
+function showName(d) {
+
+	//Location of mouse	
+	var xLoc = d3.mouse(this)[0];
+	var yLoc = d3.mouse(this)[1];
+	
+	tooltip.attr("transform", "translate(" + xLoc + "," + yLoc + ")");
+	tooltip.select("text").text(d.body);
+}//showName
+
+//Update the radius
+function rescale() {
+	radiusScale = Math.pow($("#slider").slider("value"),2);
+	
+	force.stop();
+		d3.selectAll(".solarObject")
+			.transition()
+			.attr("r", function(d) { return radiusScale*d.radius; })
+			.attr("width", function(d) {return 2*radiusScale*d.radius;})
+			.attr("height", function(d) {return 2*radiusScale*d.radius;});
+	force.start();
+}//rescale
+
+//Set up the slider
+$(function() {
+$("#slider").slider({
+  orientation: "horizontal",
+  min: 1,
+  max: 4,
+  value: 1,
+  step: 0.25,
+  slide: rescale,
+  change: rescale
+});
+});
+
+// Move d to be adjacent to the cluster node.
+function cluster(alpha) {
+  var max = {};
+
+  // Find the largest node for each cluster.
+  nodes.forEach(function(d) {
+	  if (d.type == "Star") {
+		  max = d;
+		}
+	  });
+
+	  return function(d) {
+		var node = max,
+			l,
+			r,
+			x,
+			y,
+			i = -1;
+
+		if (node == d) return;
+
+		x = d.x - node.x;
+		y = d.y - node.y;
+		l = Math.sqrt(x * x + y * y);
+		r = radiusScale* (d.radius + node.radius);
+		if (l != r) {
+		  l = (l - r) / l * alpha;
+		  d.x -= x *= l;
+		  d.y -= y *= l;
+		  node.x += x;
+		  node.y += y;
+		}
+	  };
+}//cluster
+		
+// Resolves collisions between d and all other circles.
+function collide(alpha) {
+  var quadtree = d3.geom.quadtree(nodes);
+  return function(d) {
+	//var r = radiusScale*(d.radius + radius.domain()[1]) + padding,
+	var r = radiusScale*(d.radius + maxSize) + padding,
+		nx1 = d.x - r,
+		nx2 = d.x + r,
+		ny1 = d.y - r,
+		ny2 = d.y + r;
+	quadtree.visit(function(quad, x1, y1, x2, y2) {
+	  if (quad.point && (quad.point !== d)) {
+		var x = d.x - quad.point.x,
+			y = d.y - quad.point.y,
+			l = Math.sqrt(x * x + y * y),
+			r = radiusScale*(d.radius + quad.point.radius);
+		if (l < r) {
+		  l = (l - r) / l * alpha;
+		  d.x -= x *= l;
+		  d.y -= y *= l;
+		  quad.point.x += x;
+		  quad.point.y += y;
+		}
+	  }
+	  return x1 > nx2
+		  || x2 < nx1
+		  || y1 > ny2
+		  || y2 < ny1;
+	});
+  };
+}//collide
---
+++ js/mainCode.js
@@ -0,0 +1,108 @@
+//Width and Height of the SVG
+var wind = window,
+	d = document,
+	e = d.documentElement,
+	g = d.getElementsByTagName('body')[0],
+	x = wind.innerWidth || e.clientWidth || g.clientWidth,
+	y = wind.innerHeight|| e.clientHeight|| g.clientHeight;
+
+//SVG margins
+var margin = {top: 0, right: 0, bottom: 0, left: 0},
+	otherDivHeights = 150,
+	width = x - margin.left - margin.right - 30,
+	height = y - margin.top - margin.bottom - 30 - otherDivHeights;
+
+//Initiate SVG
+var svg = d3.select("#chart").append("svg")
+			.attr("class","chartWrapper")
+			.attr("width", width + margin.left + margin.right)
+			.attr("height", height + margin.top + margin.bottom)
+		  .append("g")
+			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+//Container for all SVG patterns			
+var patternContainer = svg.append("g").attr("class","patternContainer");
+
+//Container for all the planets and Sun
+var planetContainer = svg.append("g").attr("class","planetContainer");
+
+//Show planet name on hover over
+var tooltip = svg.append("g")
+				.attr("class", "tooltip")
+				.style("display", "none");
+	  
+var m = 3, //Number of types of objects
+	padding = 5,
+	radiusScale = 1,
+	force,
+	nodes,
+	circle,
+	maxSize,
+	labels;
+			
+d3.csv("Solar planets object sizes.csv", function(error, data) {
+
+	//Convert to numeric values
+	//Note that the value for the radius of Saturn should be 9.14
+	//The value of 21.56 is the value needed to include the rings without clipping
+	data.forEach(function(d) {
+		d.meanRadiusEarth = +d.meanRadiusEarth;
+	});
+	
+	//Save the size of the sun
+	maxSize = d3.max(data, function(d) {return d.meanRadiusEarth;})
+
+	//Create dataset for planets
+	nodes = d3.range(data.length).map(function(d,i) {
+	  return {
+		radius: data[i].meanRadiusEarth,
+		body: data[i].body,
+		type: data[i].type,
+		imgsrc: data[i].imgsrc
+	  };
+	});
+
+	force = d3.layout.force()
+		.nodes(nodes)
+		.size([width, height])
+		.gravity(.02)
+		.charge(0)
+		.on("tick", tick)
+		.start();
+
+	//Create pattern fill for each planet
+	patternContainer
+        .selectAll("defs").data(nodes).enter()
+		.append("defs")
+			.append('pattern')
+			.attr('id', function(d){return "planet-"+d.body;})
+			.attr('class', "pattern")
+			.attr('patternContentUnits', 'objectBoundingBox')
+			.attr('width', 1)
+			.attr('height', 1)
+		   .append("image")
+			.attr('class', "patternImage")
+			.attr("xlink:href", function(d){return d.imgsrc;})
+			.attr('width', 1)
+			.attr('height', 1);
+   
+	//Initiate the planets
+	circle = planetContainer.selectAll("circle")
+		.data(nodes)
+	  .enter().append("circle")
+		.attr("class", "solarObject")
+		.attr("r", function(d) { return radiusScale*d.radius; })
+        .attr("cx",0)
+        .attr("cy",0)
+		.style("fill", function(d){return "url(#planet-" + d.body + ")";})
+		.on("mouseover", function() { tooltip.style("display", null); })
+		.on("mouseout", function() { tooltip.style("display", "none"); })
+		.on("mousemove", showName)
+		.call(force.drag);
+
+	//Tooltip like label
+	tooltip.append("text")
+		  .attr("x", 9)
+		  .attr("dy", ".35em");
+	  
+});//csv
